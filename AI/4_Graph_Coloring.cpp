2d0f250d83c854cf7820df30b7ec31bb70959612
kartik9436 <kartickpatil00@gmail.com>
2025-05-07 18:43:25 +0530

    Update 4_Graph_Coloring.cpp

--- AI/4_Graph_Coloring.cpp
+++ AI/4_Graph_Coloring.cpp
@@ -4,18 +4,15 @@ using namespace std;
 
 void addEdge(int graph[5][5], int edge1, int edge2)
 {
-    for (int i = 0; i < 5; i++)
-    {
-        graph[edge1][edge2] = 1;
-        graph[edge2][edge1] = 1;
-    }
+    graph[edge1][edge2] = 1;
+    graph[edge2][edge1] = 1;
 }
 
-bool safeToAssign(int i, int j, int graph[5][5], int v, vector<int> color)
+bool safeToAssign(int i, int colorToAssign, int graph[5][5], int v, const vector<int>& color)
 {
     for (int k = 0; k < v; k++)
     {
-        if (graph[i][k] == 1 && color[k] == j)
+        if (graph[i][k] == 1 && color[k] == colorToAssign)
         {
             return false;
         }
@@ -23,20 +20,22 @@ bool safeToAssign(int i, int j, int graph[5][5], int v, vector<int> color)
     return true;
 }
 
-bool function(int graph[5][5], int m, int v, int i, vector<int> color)
+bool assignColors(int graph[5][5], int m, int v, int i, vector<int>& color)
 {
     if (i == v)
     {
+        // All vertices assigned
         return true;
     }
-    for (int j = 0; j < m; j++)
+
+    for (int j = 1; j <= m; j++) // use colors 1 to m
     {
         if (safeToAssign(i, j, graph, v, color))
         {
             color[i] = j;
-            if (function(graph, m, v, i + 1, color))
+            if (assignColors(graph, m, v, i + 1, color))
                 return true;
-            color[i] = -1;
+            color[i] = 0; // backtrack
         }
     }
     return false;
@@ -44,25 +43,37 @@ bool function(int graph[5][5], int m, int v, int i, vector<int> color)
 
 bool graphColoring(int graph[5][5], int m, int v)
 {
-
-    vector<int> color = {v, -1};
-    int i = 0;
-    return function(graph, m, v, i, color);
+    vector<int> color(v, 0); // initialized with 0 (no color)
+    if (assignColors(graph, m, v, 0, color))
+    {
+        cout << "Coloring possible with " << m << " colors:\n";
+        for (int i = 0; i < v; i++)
+        {
+            cout << "Vertex " << i << " ---> Color " << color[i] << "\n";
+        }
+        return true;
+    }
+    else
+    {
+        cout << "Coloring NOT possible with " << m << " colors.\n";
+        return false;
+    }
 }
 
 int main()
 {
-    int m = 3; // no. of colors
-    int v = 5; // no. of vertices
-    int graph[v][v] = {0};
+    int m = 3; // number of colors
+    int v = 5; // number of vertices
+    int graph[5][5] = {0};
 
     addEdge(graph, 0, 1);
     addEdge(graph, 0, 2);
     addEdge(graph, 0, 3);
     addEdge(graph, 2, 4);
-    addEdge(graph, 2, 5);
-    addEdge(graph, 3, 5);
+    // Edge to vertex 5 removed because only 0–4 are valid
+    addEdge(graph, 3, 4); // Use valid vertex
 
     graphColoring(graph, m, v);
+
+    return 0;
 }
-/*This C++ code solves the Graph Coloring Problem using backtracking, where the goal is to assign colors to graph vertices such that no two adjacent vertices share the same color, using at most m colors. The graph is represented using an adjacency matrix, and the function safeToAssign() ensures a color can be safely given to a vertex. The recursive function function() tries assigning each color to vertices one by one and backtracks if a conflict arises. The graphColoring() function initializes the color list and starts the coloring process. However, there are bugs: the addEdge() function unnecessarily loops, the color vector is incorrectly initialized, and some vertex indices are out of bounds (e.g., vertex 5 in a 5-vertex graph). Fixing these issues will make the code functional for checking if valid coloring exists for the given graph.*/
